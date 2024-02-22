b5ceb56527cdfb8d893d58a340310b684f251398
Billiam <billiamthesecond@gmail.com>
2024-02-21 18:06:30 -0600

    Allow anchor links without href attributes

--- template/build/strip-limited-attributes.js
+++ template/build/strip-limited-attributes.js
@@ -4,7 +4,7 @@ export default (tree) =>
     // id attributes not allowed
     delete node?.attrs?.id
     // only http links allowed
-    if (node.tag === 'a' && !node?.attrs?.href.startsWith('http')) {
+    if (node.tag === 'a' && node.attrs?.href && !node.attrs.href.startsWith('http')) {
       delete node.attrs.href
     }
     // some tags not allowed
