cde36fbfa11a7072e62bcb54a46b614c193e79c6
Billiam <billiamthesecond@gmail.com>
2024-02-24 16:48:55 -0600

    Escape blacklisted nodes instead of removing, support anchor prefixing

--- template/build/strip-limited-attributes.js
+++ template/build/strip-limited-attributes.js
@@ -1,15 +1,30 @@
-const blacklistedTags = ['script', 'link', 'picture']
-export default (tree) =>
+const blacklistedTags = ['script', 'link', 'picture', 'nav', 'section', 'article', 'aside', 'header', 'footer']
+
+export default ({ pageUrl } = {}) => (tree) =>
   tree.walk((node) => {
     // id attributes not allowed
     delete node?.attrs?.id
+
     // only http links allowed
-    if (node.tag === 'a' && node.attrs?.href && !node.attrs.href.startsWith('http')) {
-      delete node.attrs.href
+    if (node.tag === 'a' && node.attrs?.href) {
+      if (pageUrl && node.attrs.href.startsWith('#')) {
+        node.attrs.href = pageUrl + node.attrs.href
+      } else if (!node.attrs.href.startsWith('http')) {
+        delete node.attrs.href
+      }
     }
+
     // some tags not allowed
     if (blacklistedTags.includes(node.tag)) {
-      return
+      const tagAttributes = Object.entries(node.attrs).map(([key, val]) => {
+        return `${key}="${val}"`
+      }).join(' ')
+
+      return [
+        `&lt;${node.tag}${tagAttributes ? ' ' + tagAttributes: ''}&gt;`,
+        ...node.content,
+        `&lt;/${node.tag}&gt;`
+      ]
     }
     return node
   })
