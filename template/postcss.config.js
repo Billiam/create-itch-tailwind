ccbe876e2b46bf9bc6d1d52ac79dcae219f81ab2
Billiam <billiamthesecond@gmail.com>
2024-02-26 23:28:40 -0600

    Vendoc postcss-prefixer to:
    * Run at the end of processing so that other plugins that add rules can also be prefixed
    * Match ignored selectors exactly, instead of testing for inclusion

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,11 +1,12 @@
 import { templateClasses } from './build/template-classes.cjs'
+const normalizedClasses = Array.from(templateClasses).map(className => `.${className}`)
 
 export default {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
     tailwindcss: {},
-    'postcss-prefixer': {
+    './build/postcss/prefixer/prefixer.cjs': {
       prefix: 'custom-',
       ignore: [...Array.from(templateClasses), /^custom-/]
     },
